004c1912aec79de49320ec970dae96ede35616d1
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-07-06 12:07:12 +0200

    ass Creation page component

---
+++ src/components/creation/Creation.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Creation extends React.Component {
+    render() {
+        return (
+            <div>
+                Creation page!
+            </div>
+        )
+    }
+}
+
+export default Creation;

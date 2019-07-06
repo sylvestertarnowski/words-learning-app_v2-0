0ce6cab8ebc586c3a609081158c39d096a3868e3
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-07-06 12:21:30 +0200

    add Screen Change management logic to state of Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
 import NavBar from './NavBar';
 import Lists from './lists/Lists';
+import Creation from './creation/Creation';
+import Learning from './learning/Learning';
+
+type S = {
+    currentScreen: 'lists' | 'creation' | 'learning';
+}
 
 class Main extends React.Component {
+    readonly state = {
+        currentScreen: "lists",
+    } as S;
+
+    readonly screens = {
+        lists: <Lists />,
+        creation: <Creation />,
+        learning: <Learning />,
+    }
+
+    changeScreen = (screen: S["currentScreen"]) => {
+        this.setState({
+            currentScreen: screen,
+        })
+    }
+
     render() {
+        const currentScreen = this.screens[this.state.currentScreen];
+
         return (
             <div className="main-container">
                 <NavBar />
-                <Lists />
+                {currentScreen}
             </div>
         )
     }
---
+++ src/components/learning/Learning.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Learning extends React.Component {
+    render() {
+        return (
+            <div>
+                Learning !!!!
+            </div>
+        )
+    }
+}
+
+export default Learning;

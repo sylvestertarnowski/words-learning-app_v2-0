ad98b5175d47a9c89fce73c97cc0633443839362
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-07-28 15:18:37 +0200

    add focus change to first input field after previous words were added, add Context.Consumer for future translations

--- src/components/creation/Creation.tsx
+++ src/components/creation/Creation.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { MyContext } from '../Context';
 
 type S = {
     title: string,
@@ -14,57 +15,68 @@ const Creation: React.FC = () => {
     const [myWords, setMyWords] = useState<S['words']>([]);
     const [nextWord, setNextWord] = useState("");
     const [nextTranslation, setNextTranslation] = useState("");
-    
+    let inputWord = useRef<HTMLInputElement>(null);
+
     useEffect(() => {
         setNextWord("");
         setNextTranslation("");
+        inputWord.current && inputWord.current.focus();
     }, [myWords])
 
     return (
-        <div>
-            <h3>Title</h3>
-            <input 
-                placeholder="List title"
-                value={myTitle}
-                onChange={
-                    ({ target: { value } }) => setMyTitle(value)
-                }
-            />
-            <h3>Words</h3>
-            <ul>
-                {myWords && myWords.map((item) => <li key={item.word + item.translation}>
-                    {item.word} - {item.translation}
-                </li>)}
-            </ul>
-            <form
-                onSubmit={
-                    (e) => {
-                        e.preventDefault();
-                        setMyWords([
-                            ...myWords,
-                            {
-                                word: nextWord,
-                                translation: nextTranslation,
+        <MyContext.Consumer>
+            {(context) => {
+                return (
+                    <div>
+                        <h3>Title</h3>
+                        <input
+                            placeholder="List title"
+                            value={myTitle}
+                            onChange={
+                                ({ target: { value } }) => setMyTitle(value)
                             }
-                        ])
-                    }
-                }
-            >
-                <input
-                    placeholder="Word"
-                    value={nextWord}
-                    onChange={({ target: { value } }) => setNextWord(value)}
-                />
-                <input
-                    placeholder="Translation"
-                    value={nextTranslation}
-                    onChange={({ target: { value } }) => setNextTranslation(value)}
-                />
-                <button>
-                    Add
+                        />
+                        <h3>Words</h3>
+                        <ul>
+                            {myWords && myWords.map((item) => <li key={item.word + item.translation}>
+                                {item.word} - {item.translation}
+                            </li>)}
+                        </ul>
+                        <form
+                            onSubmit={
+                                (e) => {
+                                    e.preventDefault();
+                                    setMyWords([
+                                        ...myWords,
+                                        {
+                                            word: nextWord,
+                                            translation: nextTranslation,
+                                        }
+                                    ])
+                                }
+                            }
+                        >
+                            <input
+                                ref={inputWord}
+                                type="text"
+                                placeholder="Word"
+                                value={nextWord}
+                                onChange={({ target: { value } }) => setNextWord(value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Translation"
+                                value={nextTranslation}
+                                onChange={({ target: { value } }) => setNextTranslation(value)}
+                            />
+                            <button>
+                                Add
                 </button>
-            </form>
-        </div>
+                        </form>
+                    </div>
+                )
+            }}
+        </MyContext.Consumer>
     )
 }
 

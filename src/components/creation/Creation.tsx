302f59dc3e98e015a1a5eba3021890984aef3ed9
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-07-28 10:40:20 +0200

    add list creation logic, possible to add new words, list of words is displayed in the component

--- src/components/creation/Creation.tsx
+++ src/components/creation/Creation.tsx
@@ -1,13 +1,67 @@
 import * as React from 'react';
+import { useState } from 'react';
 
-class Creation extends React.Component {
-    render() {
-        return (
-            <div>
-                Creation page!
-            </div>
-        )
-    }
+type S = {
+    title: string,
+    words: {
+        word: string,
+        translation: string,
+    }[]
+}
+
+const Creation: React.FC = () => {
+    const [myTitle, setMyTitle] = useState("");
+    const [myWords, setMyWords] = useState<S['words']>([]);
+    const [nextWord, setNextWord] = useState("");
+    const [nextTranslation, setNextTranslation] = useState("");
+    
+
+    return (
+        <div>
+            <h3>Title</h3>
+            <input 
+                placeholder="List title"
+                value={myTitle}
+                onChange={
+                    ({ target: { value } }) => setMyTitle(value)
+                }
+            />
+            <h3>Words</h3>
+            <ul>
+                {myWords && myWords.map((item) => <li key={item.word + item.translation}>
+                    {item.word} - {item.translation}
+                </li>)}
+            </ul>
+            <form
+                onSubmit={
+                    (e) => {
+                        e.preventDefault();
+                        setMyWords([
+                            ...myWords,
+                            {
+                                word: nextWord,
+                                translation: nextTranslation,
+                            }
+                        ])
+                    }
+                }
+            >
+                <input
+                    placeholder="Word"
+                    value={nextWord}
+                    onChange={({ target: { value } }) => setNextWord(value)}
+                />
+                <input
+                    placeholder="Translation"
+                    value={nextTranslation}
+                    onChange={({ target: { value } }) => setNextTranslation(value)}
+                />
+                <button>
+                    Add
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default Creation;
